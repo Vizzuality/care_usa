f708eeb72b86fe3b46eb29e6b76b51b673613a24
Clara Linos <clara.linos@gmail.com>
2016-04-11 19:09:16 +0200

    styling

--- src/components/Dashboard/DashTabs.js
+++ src/components/Dashboard/DashTabs.js
@@ -9,20 +9,20 @@ class DashboardTabs extends React.Component {
     super(props);
     this.props = props;
     this.state = {};
-
-    console.log(this.props);
   }
 
   render() {
     return (
       <div className="l-dash-tabs">
         <ul className="m-dashboard-tabs">
-          <li className={ this.props.currentMap == 'donations' ? 'is-active tab' : 'tab' }>
+          <li className='tab'>
             <button
+              className={ this.props.currentMap == 'donations' && 'is-active' }
               onClick={ this.props.changeMapFn.bind(null, 'donations') }
-            >Donors</button></li>
-          <li className={ this.props.currentMap == 'projects' ? 'is-active tab' : 'tab' }>
+            >Donations</button></li>
+          <li className='tab'>
             <button
+              className={ this.props.currentMap == 'projects' && 'is-active' }
               onClick={ this.props.changeMapFn.bind(null, 'projects') }
             >Projects</button></li>
         </ul>
