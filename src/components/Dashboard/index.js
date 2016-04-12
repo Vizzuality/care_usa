550ff9ef937fecc495727b379b99b40f99a0a961
Clara Linos <clara.linos@gmail.com>
2016-04-12 12:53:14 +0200

    updated dashboard layout with latest design changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -21,6 +21,12 @@ class Dashboard extends React.Component {
           changeMapFn = { this.props.changeMapFn }
         />
         <div className="m-dashboard-panel">
+          <div className="dashboard-header">
+            <button className="text text-link">filters</button>
+            <a href="http://www.care.org/donate" rel="noreferrer" target="_blank"className="btn btn-secondary">
+              Donate
+            </a>
+          </div>
           <div className="scroll-wrapper">
             <DashSummary
               currentMap = { this.props.currentMap }
