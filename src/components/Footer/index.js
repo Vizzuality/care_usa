165644820d46b27b115c8fe825761c910e836129
bitTal <albafjez@gmail.com>
2016-04-17 22:25:28 +0200

    Footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,13 +18,13 @@ class Footer extends React.Component {
         </a>
           <div className="third-party-webs">
             <p className="text text-legend-s">Follow us</p>
-            <svg className="icon icon-facebook -primary"><use xlinkHref="#icon-facebook"></use></svg>
-            <svg className="icon icon-twitter -primary"><use xlinkHref="#icon-twitter"></use></svg>
-            <svg className="icon icon-instagram -primary"><use xlinkHref="#icon-instagram"></use></svg>
-            <svg className="icon icon-youtube -primary"><use xlinkHref="#icon-youtube"></use></svg>
-            <svg className="icon icon-googleplus -primary"><use xlinkHref="#icon-googleplus"></use></svg>
+            <a href="https://www.facebook.com/carefans"><svg className="icon icon-facebook -primary"><use xlinkHref="#icon-facebook"></use></svg></a>
+            <a href="https://twitter.com/CARE"><svg className="icon icon-twitter -primary"><use xlinkHref="#icon-twitter"></use></svg></a>
+            <a href="https://www.instagram.com/careorg"><svg className="icon icon-instagram -primary"><use xlinkHref="#icon-instagram"></use></svg></a>
+            <a href="https://www.youtube.com/careusa"><svg className="icon icon-youtube -primary"><use xlinkHref="#icon-youtube"></use></svg></a>
+            <a href="https://plus.google.com/+care"><svg className="icon icon-googleplus -primary"><use xlinkHref="#icon-googleplus"></use></svg></a>
           </div>
-          <div className="own-web text text-highlighted">Also visit our page <a href="www.care.org" className="text -primary">www.care.org</a></div>
+          <div className="own-web text text-highlighted">Also visit our page <a href="http://www.care.org" className="text -primary">www.care.org</a></div>
        </div>
       </div>
     );
