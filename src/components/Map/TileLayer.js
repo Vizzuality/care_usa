2f9c21e6bf2919df0be4b1cbba5eb9d9e3d14771
Clara Linos <clara.linos@gmail.com>
2016-04-21 13:21:19 +0200

    layer template

--- src/components/Map/TileLayer.js
+++ src/components/Map/TileLayer.js
@@ -18,10 +18,75 @@ class CreateTileLayer {
    */
   constructor(options) {
     this.options = Object.assign(defaults, options);
+
+    this.options.sqlTemplate = 'SELECT * FROM donors $WHERE';
+    this.options.filters = [{"sectors":[],"from-day":"3","from-month":"3","from-year":"2012","to-day":"2","to-month":"6","to-year":"2015","region":"NPL","from":"2012-03-03T00:00:00.000Z","to":"2015-06-02T00:00:00.000Z"}];
+  }
+
+  _getQuery() {
+    let sqlTemplate = this.options.sqlTemplate;
+    let sql;  
+
+    if (this.options.filters) {
+      sql = sqlTemplate.replace('$WHERE', this._getFiltersExp())
+    } else {
+      sql = sqlTemplate.replace('$WHERE','');
+    }
+
+    return sql;
+  }
+
+  _getFiltersExp() {
+    let filters, filtersExp;
+    let from = '';
+    let to = '';
+    let region = '';
+    let sectors = '';
+    
+    filters = this.options.filters[0];
+
+    if (filters.from) {
+      from = "date > '" + filters['from-month'] + '-' + filters['from-day'] + '-' + filters['from-year'] + "'::date ";
+
+      if (filters.to || filters.region || filters.sectors.length < 0) {
+        from = from + "AND ";
+      }
+    }
+
+    if (filters.to) {
+      to = "date < '" + filters['to-month'] + '-' + filters['to-day'] + '-' + filters['to-year'] + "'::date ";
+
+      if (filters.region || filters.sectors.length < 0) {
+        to = to + "AND ";
+      }
+    }
+
+    if (filters.region) {
+      region = "countries like '%" + filters.region + "%' ";
+
+      if (filters.sectors.length < 0) {
+        region = region + "AND ";
+      }
+    }
+
+    if (filters.sectors.length < 0) {
+      let sectorsItems = "";
+      let items = filters.sectors.length;
+
+      $.each(filters.sectors, function(i, sector) {
+        sectorsItems = sectorsItems + "'" + sector + "'";
+        if (i < (items - 1)) { sectorsItems = sectorsItems + ", " }
+      })
+
+      sectors = "sectors in ("+ sectorsItems +"); ";
+    }
+
+    return 'WHERE ' + from + to + region + sectors;
   }
 
   createLayer() {
-
+    this.options.sql = this._getQuery();
+    console.log( this.options.sql )
     const cartoAccount = this.options.cartodbAccount;
 
     // data layers parameterization
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const config = {
-  cartodbAccount: 'simbiotica',
+  cartodbAccount: 'careusa',
+  cartodbKey: 'a749652a38e93b2ade0fdbb8751007b64d32e661',
   mapboxToken: 'cGsuZXlKMUlqb2laR2hoYTJWc2FXeGhJaXdpWVNJNkluUmtPRE5tZHpBaWZRLjFhUGpSaXRYUkxPZW9jWlNaNWpxQXc=',
   apiUrl: 'https://care-api.herokuapp.com/api/v1'
 };
