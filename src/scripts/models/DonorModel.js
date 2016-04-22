cac45a8745b159982e292e0bf2c5460e666cdadf
Clara Linos <clara.linos@gmail.com>
2016-04-22 19:19:15 +0200

    cursor pointer to map

--- src/scripts/models/DonorModel.js
+++ src/scripts/models/DonorModel.js
@@ -6,17 +6,10 @@ class DonorModel extends CartodbModel {
 
   constructor(options) {
     super(options);
-    const settings = {
-      table: 'donors',
-      query: 'iso=\'USA\''
-    }
-    // this.options = Object.assign(this.defaults, settings, options);
   }
 
   customFetch(options) {
-  	console.log('model', options);
   	const url = `${config.apiUrl}/donations?lat=${options.latLng.lat}&lng=${options.latLng.lng}`;
-  	console.log(url);
   	return this.fetch({ url: url });
   }
 
