997ec5c7d53a91eceb9f9fddaf21c46fa0989da9
Simao Belchior <simaobelchior@gmail.com>
2016-04-26 18:30:59 +0200

    Copy changes to anniversary and landing page

--- src/components/Anniversary/CaresPackage/index.js
+++ src/components/Anniversary/CaresPackage/index.js
@@ -17,7 +17,7 @@ class CaresPackage extends React.Component {
             <h1 className="text text-module-title -light">Care's Package Anniversary</h1>
             <h2 className="text text-claim -light">70 years of Lasting Change</h2>
           </header>
-          <p className="text text-highlighted -light">CARE was founded in 1945, when 22 American organizations came together to rush lifesaving CARE Packages to survivors of World War II. Thousands of Americans, including President Harry S. Truman, contributed to the effort.</p>
+          <p className="text text-highlighted -light">CARE was founded in 1945 to rush lifesaving CARE Packages to survivors of World War II. The generosity of millions of Americans turned a simple box into an icon.</p>
           <aside className="find-more">
             <a href="#" className="btn btn-primary">Find out more</a>
           </aside>
