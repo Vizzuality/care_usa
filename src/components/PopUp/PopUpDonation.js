b5a1aa8f8a950eaa2d0a34a2de197ce8934b70d1
bitTal <albafjez@gmail.com>
2016-04-27 17:22:09 +0200

    Donors modal test

--- src/components/PopUp/PopUpDonation.js
+++ src/components/PopUp/PopUpDonation.js
@@ -41,12 +41,16 @@ class PopUpDonation extends PopUp {
                 </ul>`
   }
 
+  _donorsModal() {
+    document.getElementById('donors-modal').style.display = 'inherit';
+  }
+
   _getContent() {
     const sectorsItems = (this.model.get('sectors').length > 0) ? this._getSectors() : '';
     const regionsItems = (this.model.get('countries').length > 0) ? this._getRegions() : '';
-debugger;
+
     return `<h1 class="text text-module-title -light"> ${ utils.numberNotation( (this.model.get('total_donors')) ) } donors - <span class="number-m">${ utils.numberNotation(  this.model.get('total_funds') )}$
-    </span></h1>
+    </span><span class="btn-donors" title="Donors" onclick="this._donorsModal()">+</span></h1>
     <h2 class="text text-legend-s -light">${this.model.get('location')['city']}</h2>
     ${ sectorsItems }
     ${ regionsItems }`
