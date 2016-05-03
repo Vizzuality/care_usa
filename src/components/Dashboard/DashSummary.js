7a3b716c5bdfa132af831438b100cdd77ea813c5
bitTal <albafjez@gmail.com>
2016-05-03 19:11:52 +0200

    Dashboard style fixes

--- src/components/Dashboard/DashSummary.js
+++ src/components/Dashboard/DashSummary.js
@@ -53,8 +53,8 @@ class DashSummary extends React.Component {
     return (
       <div className="m-dash-summary">
         <div className="summary-item">
-          <p className="text text-legend-title">Amount donated </p>
-          <span className="number number-l"> ${ utils.numberNotation(this.state.donationsAmount) }</span>
+          <p className="text text-legend-title">Amount donated</p>
+          <span className="number number-l"> { utils.numberNotation(this.state.donationsAmount) }</span>
         </div>
         <div className="summary-item">
           <p className="text text-legend-s">Donations </p>
