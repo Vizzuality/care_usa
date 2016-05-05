0c57c483e5e0627022fbd94b78815ecd4f5d3473
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-05 12:14:52 +0200

    Updates the layersSpec collection with information about the timeline

--- src/scripts/collections/layersCollection.js
+++ src/scripts/collections/layersCollection.js
@@ -8,6 +8,58 @@ class LayersCollection extends Backbone.Collection {
     return `${config.apiUrl}/layers`;
   }
 
+  parse(data) {
+    return data.map(layerSpec => {
+      switch(layerSpec.slug) {
+        case 'amount-of-money':
+          layerSpec.timeline = {
+            speed: 10,
+            interval: {
+              unit: 'week',
+              count: 2
+            }
+          };
+          break;
+        case 'number-of-donors':
+          layerSpec.timeline = {
+            speed: 40,
+            interval: {
+              unit: 'month',
+              count: 1
+            }
+          };
+          break;
+        case 'projects':
+          layerSpec.timeline = {
+            speed: 40,
+            interval: {
+              unit: 'year',
+              count: 1
+            }
+          };
+          break;
+        case 'refugee-assistance':
+          layerSpec.timeline = {
+            speed: 40,
+            interval: {
+              unit: 'year',
+              count: 1
+            }
+          };
+          break;
+      }
+
+      layerSpec.domain = [
+        layerSpec.start_date,
+        layerSpec.end_date
+      ];
+      delete layerSpec.start_date;
+      delete layerSpec.end_date;
+
+      return layerSpec;
+    });
+  }
+
 }
 
 export default new LayersCollection();
