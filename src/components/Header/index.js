790ba33950dd5279d74f92a6eae40443a72b301a
Clara Linos <clara.linos@gmail.com>
2016-05-10 12:53:23 +0200

    header embed settings

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,9 +7,29 @@ class Header extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      embed: false
+    };
+  }
+
+  componentDidMount() {
+    !this.state.embed ? this.isEmbed() : '';
+  }
+
+  isEmbed() {
+    const params = window.location.hash.split('&');
+    params.map(param => {
+      if (param.indexOf('embed=') !== -1) {
+        const embed = param.split('=')[1];
+        this.setState({embed});
+      }
+    });
   }
 
   render() {
+    const embedElement = this.state.embed ? '-embed-element' : '';
+
     return (
       <div id="header" className="l-header">
         <div className="wrap">
@@ -25,10 +45,13 @@ class Header extends React.Component {
                 <use xlinkHref="#icon-menu"></use>
               </svg>
             </button>
-            <ul className="menu">
-              <li><a href="/" className={ this.props.currentTab == 'who-cares' ? 'is-active menu-link' : 'menu-link' } onClick={ this.props.changePageFn.bind(null, 'who-cares') }>Donor Impact Map</a></li>
-              <li><a href="anniversary.html" className={ this.props.currentTab == 'anniversary' ? 'is-active menu-link' : 'menu-link' } onClick={ this.props.changePageFn.bind(null, 'anniversary') }>CARE’s History</a></li>
-          </ul>
+
+            { !this.state.embed ?
+              <ul className="menu">
+                <li><a href="/" className={ this.props.currentTab == 'who-cares' ? 'is-active menu-link' : 'menu-link' } onClick={ this.props.changePageFn.bind(null, 'who-cares') }>Donor Impact Map</a></li>
+                <li><a href="anniversary.html" className={ this.props.currentTab == 'anniversary' ? 'is-active menu-link' : 'menu-link' } onClick={ this.props.changePageFn.bind(null, 'anniversary') }>CARE’s History</a></li>
+              </ul>
+            }
          </div>
         </div>
       </div>
