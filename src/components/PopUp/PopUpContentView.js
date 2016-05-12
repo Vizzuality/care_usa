dbb2c631c9dfde7f1366da52b6c1d2c459f659bf
Clara Linos <clara.linos@gmail.com>
2016-05-12 12:42:43 +0200

    Fixes bug with my-donations pop up not accessing the correct option

--- src/components/PopUp/PopUpContentView.js
+++ src/components/PopUp/PopUpContentView.js
@@ -19,7 +19,9 @@ class PopUpContentView extends Backbone.View {
   }
 
   getPopUp() {
-    if (this.options.layer.slug === 'amount-of-money' || this.options.layer.slug === 'amount-of-money-torque') {
+    if (this.options.currentLayer === 'my-donation'){
+      this.currentPopUp = new PopUpMyDonation(this.options);
+    } else if (this.options.layer.slug === 'amount-of-money' || this.options.layer.slug === 'amount-of-money-torque') {
       this.currentPopUp = new PopUpDonation(this.options);
     } else if (this.options.layer.slug === 'number-of-donors') {
       this.currentPopUp = new PopUpDonationDist(this.options);
