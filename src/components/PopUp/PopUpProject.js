4b3683b3bd44992119f5c85af10d923c91c389e7
Simao Belchior <simaobelchior@gmail.com>
2016-05-13 16:37:30 +0100

    Removed refugees info from Projects' popUp. As this is no longer returned by the API

--- src/components/PopUp/PopUpProject.js
+++ src/components/PopUp/PopUpProject.js
@@ -24,25 +24,8 @@ class PopUpProject extends PopUp {
             </ul>`
   }
 
-  _getRefugeesInfo() {
-    const title = !(this.model.get('totals')['projects']) ? `<h1 class="text text-module-title -light"> ${this.model.get('location')['name']} - <span class="title-sector text text-legend-s -light">${this.model.get('crisis')[0]['name']}</span></h1>
-      <hr></hr>`: `<p class="text text-report-title -light">${this.model.get('crisis')[0]['name']}</p>`
-
-    return `
-      ${title}
-      <div class="refugees-info">
-        <span class="title-sector text text-legend-s -light">Countries with projects for refugee assistance in the country</span>
-        <ul>
-          ${ this.model.get('crisis')[0] && this.model.get('crisis')[0]['parties_involved'].length > 0 ? this.model.get('crisis')[0]['parties_involved'].map(country => `<li class="sector text text-legend-s -light"> ${country.country} </li>`).join('') : ''}
-        </ul>
-      </div>
-      <hr></hr>
-    `
-  }
-
   _getProjectsInfo() {
     const sectorsItems = (this.model.get('sectors').length > 0) ? this._getSectors() : '';
-    const refugeeTitle = this.model.get('crisis')[0] ? `<p class="title-sector text text-legend-s -light">${this.model.get('crisis')[0]['name']}</p>` : '';
     const womenText = this.model.get('totals')['women_and_girls'] ? `<p class="text-legend-s"><span class="number-m">${ utils.numberNotation(this.model.get('totals')['women_and_girls']) + '%' }</span> Women & girls</p>` : '';
 
     return `
@@ -60,12 +43,10 @@ class PopUpProject extends PopUp {
   }
 
   _getContent() {
-    const refuggesInfo = (this.model.get('crisis')[0]) ? this._getRefugeesInfo() : '';
     const projectsInfo = (this.model.get('totals')['projects']) ? this._getProjectsInfo() : '';
 
     return `<div class="wrapper -project">
               ${ projectsInfo }
-              ${ refuggesInfo }
               <a class="text text-cta -light" href=${this.model.attributes.url}>Explore country page</a>
               <svg class="icon icon-externallink -light">
                 <use xlink:href="#icon-externallink"></use>
