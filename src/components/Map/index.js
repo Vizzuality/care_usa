3f729d031852e8f377e678b34bd426d774c595c4
bitTal <albafjez@gmail.com>
2016-05-19 19:08:21 +0200

    Conditional fix

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -177,7 +177,8 @@ class MapView extends Backbone.View {
     /* We make sure that we don't ask for data outside the domain */
     if(state.timelineDate && state.layer) {
       const domain = state.layer.domain.map(date => moment.utc(date, 'YYYY-MM-DD').toDate());
-      if(!this.state.get('startDate')) {
+      
+      if(!this.state.attributes.filters.to) {
         if(+state.timelineDate < +domain[0]) state.timelineDate = domain[0];
       }
       if(+state.timelineDate > +domain[1]) state.timelineDate = domain[1];
