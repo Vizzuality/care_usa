a4b5576dece7edc51fb48ec5c609d53db16ca32c
bitTal <albafjez@gmail.com>
2016-05-23 19:16:32 +0200

    Menu device closes when clicking on background

--- src/components/MenuDevice/index.js
+++ src/components/MenuDevice/index.js
@@ -9,20 +9,25 @@ class MenuDevice extends React.Component {
     super(props);
   }
 
+
+
   render() {
     return (
       <div className={ this.props.deviceMenuOpen ?  "is-visible m-menu--device"  : "m-menu--device" }
            ref="menuDevice">
-        <button className="btn-close-menu" onClick={ this.props.toggleMenuFn } >
-          <svg className="icon icon-close">
-            <use xlinkHref="#icon-close"></use>
-          </svg>
-        </button>
-        <div className="wrap">
-          <ul className="menu">
-            <li className={ this.props.currentPage == "who-cares" && 'is-active' } ><a href="/" className='menu-link text text-module-title -dark'>Donor Impact Map</a></li>
-            <li className={ this.props.currentPage == "anniversary" && 'is-active' } ><a href="anniversary.html" className='menu-link text text-module-title -dark'>CARE’s History</a></li>
-          </ul>
+        <div className={ this.props.deviceMenuOpen ?  "is-visible background-viel"  : "background-viel" } onClick={ this.props.toggleMenuFn }></div>
+        <div className="menu-content">
+          <button className="btn-close-menu" onClick={ this.props.toggleMenuFn } >
+            <svg className="icon icon-close">
+              <use xlinkHref="#icon-close"></use>
+            </svg>
+          </button>
+          <div className="wrap">
+            <ul className="menu">
+              <li className={ this.props.currentPage == "who-cares" && 'is-active' } ><a href="/" className='menu-link text text-module-title -dark'>Donor Impact Map</a></li>
+              <li className={ this.props.currentPage == "anniversary" && 'is-active' } ><a href="anniversary.html" className='menu-link text text-module-title -dark'>CARE’s History</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     )
