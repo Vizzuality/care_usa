3e40cd3637e010a8e5de38fe4935f688876b1538
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-23 10:46:03 +0200

    Triggers the number of the slide when changed manually

--- src/components/Anniversary/Slider/index.js
+++ src/components/Anniversary/Slider/index.js
@@ -50,6 +50,9 @@ class Slider extends React.Component {
           }
         }
       ]
+    }).on('afterChange', (e, slick, currentSlide) => {
+      /* Google Analytics */
+      ga && ga('send', 'event', 'History', 'By Numbers', currentSlide + 1);
     });
   }
 
