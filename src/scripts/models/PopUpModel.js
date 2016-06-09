9fbebb71d0d78d08ed8c83cd04b86a826d736284
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-09 15:35:51 +0200

    Updates the popup to fetch the correct range and fixes wrong code

--- src/scripts/models/PopUpModel.js
+++ src/scripts/models/PopUpModel.js
@@ -9,33 +9,38 @@ class PopUpModel extends Backbone.Model {
 
   customFetch(options) {
     this.options = options;
-    return this.fetch({ url: this._getUrl(options) });
+    return this.fetch({ url: this._getUrl() });
   }
 
   _getUrl() {
-    const noFilters = filtersModel.filtersIsEmpty();
     const distributionLayerEndpoint = this.options.layer.slug === 'number-of-donors' ? '/distribution' : '';
     const refugeesEndpoint = this.options.layer.slug === 'refugee-assistance' ? '/refugees' : '';
-
     this.baseUrl = `${config.apiUrl}/${this.options.currentMode}${distributionLayerEndpoint || refugeesEndpoint }?lat=${this.options.latLng.lat}&lng=${this.options.latLng.lng}&zoom=${this.options.zoom}`;
 
-    if(this.options.timelineDate && this.options.layer) {
-      this.baseUrl = this.baseUrl + `&start_date=${moment.utc(this.options.layer.domain[0]).format('DD-MM-YYYY')}&end_date=${moment.utc(this.options.timelineDate).format('DD-MM-YYYY')}`
-    }
-
-    if (noFilters) {
-      return this.baseUrl;
-    } else {
-      return this._getUrlWithFilters();
-    }
+    return this._getUrlWithFilters();
   }
 
   _getUrlWithFilters() {
     const filters = filtersModel.toJSON();
 
-    const startDate = filters.from && `start_date = ${moment(filters.from).format('MM-DD-YYYY')}`;
+    let startDate;
+    if(this.options.currentMode === 'donations' && this.options.timelineDate) {
+      /* When the mode is donations and the layer is the SVG one (i.e.
+       * "number-of-donors"), we want the first date to be 7 days before the
+       * other one (so before the timeline date), otherwise, for the Torque
+       * layer, we want the data of the selected day. */
+      if(this.options.layer.slug === 'number-of-donors') {
+        startDate = `start_date=${moment.utc(this.options.timelineDate).subtract(7, 'days').format('DD-MM-YYYY')}`;
+      } else {
+        startDate = `start_date=${moment.utc(this.options.timelineDate).format('DD-MM-YYYY')}`;
+      }
+    }
+    else if(filters && filters.from) {
+      startDate = `start_date = ${moment.utc(filters.from).format('DD-MM-YYYY')}`;
+    }
+
     //To be coherent with what we are seeing on the map, we have to give prevalence to "timelineDate".
-    const endDate = (filters && filters.to || this.options.timelineDate) ? `endDate = ${moment.utc(this.options.timelineDate || filters && filters.to).format('MM-DD-YYYY')}` : '';
+    const endDate = `end_date = ${moment.utc(this.options.timelineDate || filters && filters.to).format('DD-MM-YYYY')}`;
     const regions = filters.region && `countries_iso=${filters['region']}`;
 
     let sectors = null;
