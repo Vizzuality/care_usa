ac07521ec45ac57e380f463ee3464efc645bf720
Simao Belchior <simaobelchior@gmail.com>
2016-07-27 16:15:36 +0100

    Allow for appending suffix elements to legend

--- src/components/Legend/index.js
+++ src/components/Legend/index.js
@@ -15,15 +15,28 @@ class Legend extends React.Component {
   render() {
     let legend = [];
 
-    JSON.parse(this.props.layerLegend).buckets.forEach( (bucket) => {
-      let style = { backgroundColor: bucket.color, borderColor: bucket.border }
+    const legendHsh = JSON.parse(this.props.layerLegend);
+
+    legendHsh.buckets.forEach(bucket => {
+      let style = { backgroundColor: bucket.color, borderColor: bucket.border };
       legend.push(
         <li className="legend-item" key={ bucket.color }>
           <span className={ bucket.slug + " bucket" } style={ style }></span>
           <span className="text text-legend-s">{ bucket.literal }</span>
         </li>
-      )
-    })
+      );
+    });
+
+    if(legendHsh.suffix !== undefined) {
+      legend.push(
+        <li className="legend-item" key="suffix">
+          <span className="bucket icon">
+            <img src={legendHsh.suffix.svg}></img>
+          </span>
+          <span className="text text-legend-s">{legendHsh.suffix.text}</span>
+        </li>
+      );
+    }
 
     return (
       <div className="m-legend">
