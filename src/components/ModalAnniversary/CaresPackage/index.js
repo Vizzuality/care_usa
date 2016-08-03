aebd7f5df2c2bcfcf439f46af41cf36618db98ee
edbrett <e.j.a.brett@gmail.com>
2016-08-03 14:13:04 +0200

    About page button fix and font styles

--- src/components/ModalAnniversary/CaresPackage/index.js
+++ src/components/ModalAnniversary/CaresPackage/index.js
@@ -29,9 +29,7 @@ class CaresPackage extends React.Component {
             <h2 className="text text-claim -light">70 Years of Lasting Change</h2>
           </header>
           <p className="text text-highlighted -light">CARE was founded in 1945 to rush lifesaving CARE Packages to survivors of World War II. The generosity of millions of Americans turned a simple box into an icon.</p>
-          <aside className="find-more">
-            <a href="http://www.care.org" className="btn btn-primary" onClick={ this.onClickFindMore.bind(this) } target="_blank">Visit care.org</a>
-          </aside>
+          <a href="http://www.care.org" className="btn btn-primary" onClick={ this.onClickFindMore.bind(this) } target="_blank">Visit care.org</a>
         </div>
       </article>
     )
