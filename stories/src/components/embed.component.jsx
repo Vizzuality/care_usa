f47ab8a7a6abc2eac9db594cdbdb6b094bee346b
j8seangel <jseangel@gmail.com>
2018-02-21 17:09:16 +0100

    Fix youtube video link

--- stories/src/components/embed.component.jsx
+++ stories/src/components/embed.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 function Embed (props) {
-  const { url } = props;
+  const { url, width, height } = props;
   if(!url) return null;
 
   return (
-    <iframe src={`${url}?origin=${window.location.origin}&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`} allowFullScreen title={`Iframe for ${url}`} frameBorder="0" width={820} height={360} />
+    <iframe
+      className="embed-component"
+      allowFullScreen title={`Iframe for ${url}`} frameBorder="0" width={width} height={height}
+      src={`${url}?origin=${window.location.origin}&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
+    />
   );
+}
+
+Embed.defaultProps = {
+  width: 820,
+  height: 360
 }
 
 export default Embed;
