6a453e28d1f4b7df19a06bb85a38c94bc7d6b1d3
j8seangel <jseangel@gmail.com>
2018-03-07 18:59:36 +0100

    Include stories link in map mobile menu

--- map/src/components/MenuDevice/index.js
+++ map/src/components/MenuDevice/index.js
@@ -23,6 +23,7 @@ class MenuDevice extends React.Component {
           <div className="wrap">
             <ul className="menu">
               <li className={ this.props.careHistory ? "who-cares" : 'who-cares is-active' } ><p className='menu-link text text-module-title -dark' onClick={ () => this.props.toggleHistory(false) }>Donor Impact Map</p></li>
+              <li className="anniversary"><a href="/stories" className='menu-link text text-module-title -dark'>Stories</a></li>
               <li className={ this.props.careHistory ? "anniversary is-active" : "anniversary" } ><p className='menu-link text text-module-title -dark' onClick={ () => this.props.toggleHistory(true) }>Learn about CARE</p></li>
             </ul>
           </div>
