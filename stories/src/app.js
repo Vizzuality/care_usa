d4779a77ca3861e540fbd18972908f2b72cff600
j8seangel <jseangel@gmail.com>
2018-03-22 10:36:08 +0000

    Include ga page track

--- stories/src/app.js
+++ stories/src/app.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 import Pages from 'pages';
 import Header from 'components/header/header';
 import Footer from 'components/footer.component';
+import ReactGA from 'react-ga';
 import './style.css';
+
+const GA_ID = process.env.ENVIRONMENT === 'production' ? 'UA-77384250-1' : '';
+
+if (GA_ID) {
+  console.log('initializing ga');
+  ReactGA.initialize(GA_ID);
+}
 
 class App extends Component {
   render() {
